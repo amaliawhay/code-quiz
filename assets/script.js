135bc1309beb80f107f9eb67f355e7e46ba31009
Amalia Hay <amaliawhay@gmail.com>
2020-06-07 21:52:30 -0700

    Still not functioning. Wrote as much logical as possible. Not sure where or how many errors

--- assets/script.js
+++ assets/script.js
@@ -1,65 +1,125 @@
-/*When the start button is pressed (call button),
-and a timer starts*/
-var timeEl = document.querySelector(".time");
-var heading = document.querySelector("#heading");
-var startBtn = document.getElementById("start");
-var homeEl = document.querySelector("#home");
-var clearEl = document.querySelector("#clear");
-var highScoreEl = document.querySelector("#viewScore");
-var landing = document.querySelector("#landingContent");
-var questions = document.querySelector("#questions");
-var questionsTitle = document.querySelector(
-  "#questionsTitle"
-);
-var choice1 = document.querySelector("#choice1");
-var choice2 = document.querySelector("#choice2");
-var choice3 = document.querySelector("#choice3");
-var choice4 = document.querySelector("#choice4");
-var endingScore = document.querySelector("#endingScore");
-var scoreText = document.querySelector("#scoreText");
-var initials = document.querySelector("#initials");
-var submit = document.querySelector("#submit");
-var answerIndicator = document.querySelector(
-  "#answerIndicator"
-);
-var viewScore = document.querySelector("#viewScore");
+/*PseudoCode:
+A. When the user presses start("click event listener"):
+    1. The timer(#timer) starts and is displayed at the top of the page
+        -variable for time (global)
+        -function for time decrement 
+        -display "none" for #intro content
+    2. The first question (#question) and selection of choices (#choice 1-4) with selectable buttons is displayed below the timer(#content) 
+        -Loop (randomized?) for questions array 
+        -variable to hold user answer
+B. When the user selects an answer
+    1. The answer is stored and compared to questions.a
+        -var for user answer
+        -If the user answer === questions.a:
+            --#answerCheck informs user of correct answer
+            --The time continues on same decrement 
+            --The user's score (var for score - should be global) increases 
+            --The next question is displayed (uses loop for question array)
+        -If the user answer !== questions.a:
+            --#answerCheck informs user of incorrect answer (and informs then of correct answer?)
+            --The timer loses 10 sec off the clock
+            --The user's score us unchanged
+            --The next question is displayed (uses loop for question array)
+C.a. When the timer hits 0:
+    1.No more questions are displayed
+    2.The user's score is displayed (#scoreText)
+    3.The user can enter their initials (#initials) next to their score
+        -This high score information is stored locally when user hits submit button (#submit)
+C.b. When there are no questions left, but there is time left on the clock:
+    1.No more questions are displayed
+    2.The user's score is displayed (#scoreText)
+    3.The user can enter their initials (#initials) next to their score
+        -This high score information is stored locally when user hits submit button (#submit)*/
 
-var savedArray =
-  JSON.parse(localStorage.getItem("highscore")) || [];
-
-var time = 0;
+var timerEl = document.querySelector("#timer");
+var startBtn = document.querySelector("#start");
+var intro = document.querySelector("#intro");
+var questionTitle = document.querySelector(
+  "#questionTitle"
+);
+var choice1Btn = document.querySelector("#choice1");
+var choice2Btn = document.querySelector("#choice2");
+var choice3Btn = document.querySelector("#choice3");
+var choice4Btn = document.querySelector("#choice4");
+var answerCheckEl = document.querySelector("#answerCheck");
+var finalScoreEl = document.querySelector("#finalScore");
+var scoreTextEl = document.querySelector("#scoreText");
+var initialsEl = document.querySelector("#initials");
+var submitBtn = document.querySelector("#submit");
+var secLeft = 120;
 var score = 0;
-var currentAnswer = "";
-var index = 0;
-var startTimer = 0;
+
+var userChoice = [];
 var scoreObj = {
   initials: "",
   score: 0,
 };
 
-//score function
-highScore.addEventListener("click");
-
-//This will make the back button clear the questions and high score at the end of the game
-homeEl.addEventListener("click", function () {
-  landing.style.display = "block";
-  heading.style.display = "block";
-  highScore.style.display = "none";
-});
-
-//This will clear the high score log when clicked
-clearEl.addEventListener("click", function () {
-  localStorage.clear();
-  savedArray = [];
-});
-
-//This will start the quiz (ref startQuiz function)
+var savedArray =
+  JSON.parse(localStorage.getItem("highscore")) || [];
+var questions = [
+  {
+    q: "What are the 3 fundamental languages of the web?",
+    c: [
+      "German, French, Mandarin",
+      "HTML, CSS & JavaScript",
+      "Piglatin, Gibberish, baby talk",
+      "Robot, Physics, Calculus",
+    ],
+    a: "HTML, CSS & JavaScript",
+  },
+  {
+    q:
+      "Which famous poet's daughter is considered the first computer programmer?",
+    c: [
+      "Shakespeare (Judith Quiney)",
+      "Lord Byron (Ada Lovelace)",
+      "Sylvia Plath (Frieda Hughes)",
+      "E.E. Cummings (Nancy Thayer Andrews",
+    ],
+    a: "Lord Byron (Ada Lovelace)",
+  },
+  {
+    q: "What is the definition of pseudocoding?",
+    c: [
+      "A description of the system operations written in layman's terms",
+      "Fake code designed to confuse hackers",
+      "Website templates",
+      "Bad Code",
+    ],
+    a:
+      "A description of the system operations written in layman's terms",
+  },
+  {
+    q:
+      "Which of the following is the correct syntax for JQuery?",
+    c: ["#", "%", "@", "$"],
+    a: "$",
+  },
+];
+//These are the click events for start button
 startBtn.addEventListener("click", function () {
-  landing.style.display = "none";
-  questions.style.display = "block";
-  startQuiz();
+  timerEl.textContent = setTime();
+  intro.style.display = "none";
+  questionTitle.textConent = getQuestions();
 });
-
+//These are the click events for each choice button
+choice1Btn.addEventListener(
+  "click",
+  userChoice.push(questions.c[0])
+);
+choice2Btn.addEventListener(
+  "click",
+  userChoice.push(questions.c[1])
+);
+choice3Btn.addEventListener(
+  "click",
+  userChoice.push(questions.c[2])
+);
+choice4Btn.addEventListener(
+  "click",
+  userChoice.push(questions.c[3])
+);
 //This will allow the user to store (locally) the final score with initials when the submit button is clicked
 submit.addEventListener("click", function () {
   scoreObj.initials = initials.value;
@@ -71,122 +131,56 @@ submit.addEventListener("click", function () {
   );
 });
 
-//function to run timer and fill screen with questions and choices
-function startQuiz() {
-  time = 125;
-  index = 0;
+//function to run timer
+function setTime() {
+  var timerInterval = setInterval(function () {
+    secLeft--;
+    timerEl.textContent = "Timer: " + secLeft;
 
-  shuffle(questionsArray);
-  //calls on randomization of questions function
-
-  populateQuestions(index);
-  //calls on function to put questions on screen
-
-  startTimer = setInterval(function () {
-    time--;
-    timerUpdate();
-    //displays countdown
-    timeUp();
-    //stops clock & ends quiz
+    if (secLeft === 0) {
+      clearInterval(timerInterval);
+    }
   }, 1000);
-  choice1.addEventListener("click", evaluate);
-  choice2.addEventListener("click", evaluate);
-  choice3.addEventListener("click", evaluate);
-  choice4.addEventListener("click", evaluate);
-  //allows choices to be clicked on
 }
-
-//fills div with question
-function populateQuestions(index) {
-  var temp = questionsArray[index];
-  currentAnswer = temp.a;
-  //finds correct answer from question object
-  questionsTitle.textContent = temp.q;
-  //fills question from question object
-
-  var choiceTemp = [];
-  //holds current temp
-  var i = 0;
-  //this will loop through the choices for the current question object
-  for (i; i < temp.c.length; i++) {
-    //This will display the choices available for the current question
-    choiceTemp.push(temp.c[i]);
+//function to fill questions
+function getQuestions() {
+  {
+    for (var i = 0; i < questions.length; i++);
+    var currentQ = questions[i].q;
   }
-  choice1.textContent = choiceTemp[0];
-  choice2.textContent = choiceTemp[1];
-  choice3.textContent = choiceTemp[2];
-  choice4.textContent = choiceTemp[3];
+
+  questionTitle.textContent = currentQ;
 }
 
-function timeUp() {
-  if (time < 0) {
-    //clearInterval(startTimer);(need to make this function still)
-    time = 0;
-    timerUpdate();
-    //endQuiz();(need to make this function still)
+function getChoices() {
+  var currentChoices = [];
+  for (var j = 0; j < questions.c.length; j++) {
+    currentChoices.push(questions.c[j]);
+  }
+  choice1Btn.textContent = currentChoices[0];
+  choice2Btn.textContent = currentChoices[1];
+  choice3Btn.textContent = currentChoices[2];
+  choice4Btn.textContent = currentChoices[3];
+}
+
+function checkAnswer() {
+  if (userChoice === questions.a) {
+    answerCheckEl.textContent = "Correct!";
+    score++;
   } else {
-    return;
+    answerCheckEl.textContent = "Incorrect";
+    score++;
+    time--;
+  }
+  getQuestions();
+}
+
+function endQuiz() {
+  if ((secLeft = 0)) {
+    scoreTextEl.textContent = score;
+    initialsEl.textContent = initials.value;
+  } else if (questions[4].q) {
+    scoreTextEl.textContent = score;
+    initialsEl.textContent = initials.value;
   }
 }
-
-function timerUpdate() {
-  timeEL.textContent = "Time: " + time;
-}
-
-function shuffle(array) {
-  array.sort(() => Math.random() - 0.5);
-}
-/*To do:
--Score Counter function: 
-  --variable for user input (not sure if evaluate in startTimer function comes into play here?)
-  --if user input === current answer,
-    ---populateQuestions
-    ---score++
-  --else user input !== current answer
-    ---time-- (X10?)
-    ---populateQuestions
-
--clearInterval (need to look this one up)
--endQuiz:
-  --if no more questions left,
-      ---stop time
-      ---display:
-        endingScore.textContent
-        scoreText.textContent
-        intials.textContent
-  
-
-
-// //this is the variable for all 5 questions.
-// var questions = [
-//   "What are the 3 fundamental languages of the web?",
-//   "Which famous poet's daughter is considered the first computer programmer?",
-//   //does a t/f need a variable for the answer? boolean? Can I make that variable
-//   "True or false? A person who knows how to code can read robots mind's",
-//   "What is the definition of pseudocoding?",
-//   "Which of the following is the correct syntax for JQuery?",
-// ];
-
-// //Variables for answers. Each question has its own array of answers.
-// var answers = {
-//   languageA: [
-//     "wrong",
-//     "wrong",
-//     "HTML, CSS & JavaScript",
-//     "wrong",
-//   ],
-//   poetA: [
-//     "wrong",
-//     "Lord Byron (Ada Lovelace)",
-//     "wrong",
-//     "wrong",
-//   ],
-//   pseudocodeA: [
-//     "wrong",
-//     "A description of the system operations written in layman's terms",
-//     "wrong",
-//     "wrong",
-//   ],
-//   syntaxA: ["wrong", "wrong", "$", "wrong"],
-// };
-
